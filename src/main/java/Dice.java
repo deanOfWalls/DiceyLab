d205feca1c3dcfbc04e03ba54dca0b728131e926
deanOfWalls <deanofwalls@gmail.com>
2023-03-29 23:33:24 -0400

    checkpointing

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,14 @@
+import java.util.Random;
+
 public class Dice {
+    public static void main(String[] args) {
+
+        int min = 2;
+        int max = 12;
+        Random random = new Random();
+        int randomRoll = random.nextInt((max - min) + 1) + min;
+
+    }
 
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 public class Simulation {
 
+    public void getBinsTest() {
+        Bins results = new Bins(2, 12); // for bins from 2..12
+        Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
+        results.incrementBin(10); // should increment bin # 10
+
+    }
 
 
 }
